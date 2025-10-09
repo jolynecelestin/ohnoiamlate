343eff9d9fb0c917c8ff390f0935890badb6572f
jolyne_celestin <jolynecelestin@gmail.com>
2025-10-09 12:57:30 -0400

    B: Beta files

---
+++ beta/newmove.py
@@ -0,0 +1,58 @@
+import os
+import platform
+
+def clear_screen():
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+rooms = {
+        'Master bedroom': {'North': 'Hallway 2'},
+        'Hallway 2': {'North': 'Closet', 'South': 'Master bedroom', 'East': 'Hallway 1', 'West': 'Nursery'},
+        'Nursery': {'East': 'Hallway 2'},
+        'Closet': {'South': 'Hallway 2'},
+        'Hallway 1': {'North': 'Bathroom', 'South': 'Living room', 'East': 'Bedroom', 'West': 'Hallway 2'},
+        'Bathroom': {'South': 'Hallway 1'},
+        'Bedroom': {'West': 'Hallway 1'},
+        'Living room': {'North': 'Hallway 1', 'South': 'Entryway', 'East': 'Dining room'},
+        'Dining room': {'South': 'Kitchen', 'West': 'Living room'},
+        'Kitchen': {'North': 'Dining room', 'West': 'Entryway'},
+        'Entryway': {'North': 'Living room', 'South': 'Outside', 'East': 'Kitchen', 'West': 'Garage'},
+        'Garage': {'East': 'Entryway'},
+        'Outside' : {}
+}
+
+clear_screen()
+print('\nOh no you woke up late! Move between rooms using North, South, East or West. Time is running out!')
+
+print("\n(Tip: if you want to quit the game at any point, type 'quit' and then press the [enter] key.)")
+
+current_room = 'Master bedroom'
+
+
+while True:
+
+    print(f"\nYou are in the {current_room}.")
+    exits = rooms[current_room]
+    print("\nExits:", ", ".join(exits.keys()) or "None")
+    direction = input('North, South, East or West? ').strip().capitalize()
+    clear_screen()
+
+    if direction in ('Quit', 'quit','Q','q'):
+        print('\nGame is ending now, hope you enjoyed it!')
+        print('\nall done!\n')
+        break
+    
+    if direction not in ('North', 'South', 'East', 'West'):
+        print("\nPlease enter a valid direction (North/South/East/West).")
+        continue
+    elif direction in rooms[current_room]:
+        current_room = rooms[current_room][direction]
+    else:
+         print('\nOops, bumped into a wall.')
+
+    if current_room == 'Outside':
+        print('\nYou made it outside in time! YOU WIN!')
+        print('\nall done!\n')
+        break
---
+++ beta/wifemove.py
@@ -0,0 +1,58 @@
+import os
+import platform
+
+def clear_screen():
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+rooms = {
+        'Master bedroom': {'Up': 'Hallway 2'},
+        'Hallway 2': {'Up': 'Closet', 'Down': 'Master bedroom', 'Right': 'Hallway 1', 'Left': 'Nursery'},
+        'Nursery': {'Right': 'Hallway 2'},
+        'Closet': {'Down': 'Hallway 2'},
+        'Hallway 1': {'Up': 'Bathroom', 'Down': 'Living room', 'Right': 'Bedroom', 'Left': 'Hallway 2'},
+        'Bathroom': {'Down': 'Hallway 1'},
+        'Bedroom': {'Left': 'Hallway 1'},
+        'Living room': {'Up': 'Hallway 1', 'Down': 'Entryway', 'Right': 'Dining room'},
+        'Dining room': {'Down': 'Kitchen', 'Left': 'Living room'},
+        'Kitchen': {'Up': 'Dining room', 'Left': 'Entryway'},
+        'Entryway': {'Up': 'Living room', 'Down': 'Outside', 'Right': 'Kitchen', 'Left': 'Garage'},
+        'Garage': {'Right': 'Entryway'},
+        'Outside' : {}
+}
+
+clear_screen()
+print('\nOh no you woke up late! Move between rooms using Up, Down, Right or Left. Time is running out!')
+
+print("\n(Tip: if you want to quit the game at any point, type 'quit' and then press the [enter] key.)")
+
+current_room = 'Master bedroom'
+
+
+while True:
+
+    print(f"\nYou are in the {current_room}.")
+    exits = rooms[current_room]
+    print("\nExits:", ", ".join(exits.keys()) or "None")
+    direction = input('Up, Down, Right or Left? ').strip().capitalize()
+    clear_screen()
+
+    if direction in ('Quit', 'quit','Q','q'):
+        print('\nGame is ending now, hope you enjoyed it!')
+        print('\nall done!\n')
+        break
+    
+    if direction not in ('Up', 'Down', 'Right', 'Left'):
+        print("\nPlease enter a valid direction (Up/Down/Right/Left).")
+        continue
+    elif direction in rooms[current_room]:
+        current_room = rooms[current_room][direction]
+    else:
+         print('\nOops, bumped into a wall.')
+
+    if current_room == 'Outside':
+        print('\nYou made it outside in time! YOU WIN!')
+        print('\nall done!\n')
+        break
